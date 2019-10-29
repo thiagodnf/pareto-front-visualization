473359da0f05fe7f890a79643b32142526f127c4
Thiago Nascimento <thiagodnf@gmail.com>
2019-10-29 00:48:33 -0300

    improvemens

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -19,12 +19,12 @@ class NavigationBar extends Component {
                         <Nav className="mr-auto">
                             <Nav.Link href="/">Home</Nav.Link>
                             <NavDropdown title="Examples" id="collasible-examples">
-                                <NavDropdown.Item href="/?url=/examples/zdt/zdt1.txt&hasHeader=0">ZDT 1</NavDropdown.Item>
-                                <NavDropdown.Item href="/?url=/examples/zdt/zdt2.txt&hasHeader=0">ZDT 2</NavDropdown.Item>
-                                <NavDropdown.Item href="/?url=/examples/zdt/zdt3.txt&hasHeader=0">ZDT 3</NavDropdown.Item>
+                                <NavDropdown.Item href="?url=/examples/zdt/zdt1.txt&hasHeader=0">ZDT 1</NavDropdown.Item>
+                                <NavDropdown.Item href="?url=/examples/zdt/zdt2.txt&hasHeader=0">ZDT 2</NavDropdown.Item>
+                                <NavDropdown.Item href="?url=/examples/zdt/zdt3.txt&hasHeader=0">ZDT 3</NavDropdown.Item>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="/?url=/examples/dtlz/DTLZ1.2D.pf.txt&hasHeader=0&separator=tab">DTLZ1 (2D)</NavDropdown.Item>
-                                <NavDropdown.Item href="/?url=/examples/dtlz/DTLZ1.3D.pf.txt&hasHeader=0&separator=tab">DTLZ1 (3D)</NavDropdown.Item>
+                                <NavDropdown.Item href="?url=/examples/dtlz/DTLZ1.2D.pf.txt&hasHeader=0&separator=tab">DTLZ1 (2D)</NavDropdown.Item>
+                                <NavDropdown.Item href="?url=/examples/dtlz/DTLZ1.3D.pf.txt&hasHeader=0&separator=tab">DTLZ1 (3D)</NavDropdown.Item>
                             </NavDropdown>
                             <Nav.Link href="https://github.com/thiagodnf/pareto-front-visualization" target="_blank">About</Nav.Link>
                         </Nav>
